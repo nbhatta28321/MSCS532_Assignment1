4826b02bdf5b9831d1b775196da1ae4c26a3550e
nbhatta28321 <nbhatta28321@ucumberlands.edu>
2024-08-27 15:54:07 -0400

    added comment

--- InsertionSort.py
+++ InsertionSort.py
@@ -1,15 +1,19 @@
 def insertion_sort_decresing_order(A, n):
 
-    for i in range(0, n):
-        current = A[i]
-        j=i
+    for i in range(0, n): # loop through each index in the Array list
+        current = A[i] # created a variable to point to the current value for sorting
+        j=i #assign the first value to variable
 
-        while j > 0 and current > A[j-1]:
-            temp = A[j-1]
+    # run the loop until previous value is grater than current for descending order.
+        while j > 0 and current > A[j-1]: 
+            #swap the value and point the index of j to current value
+            temp = A[j-1] 
             A[j-1] = A[j]
             A[j] = temp
             j=j-1                        
   
     print(A)
 
+
+# function to do the insertion sort and print the value
 insertion_sort_decresing_order([23,4,11,55,2,9,777], 7)
