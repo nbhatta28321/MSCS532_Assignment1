45272c7879f31d05429ee8db89f189a4c53b60d5
nbhatta28321 <nbhatta28321@ucumberlands.edu>
2024-08-27 15:30:55 -0400

    added insertion sort algorithm

--- InsertionSort.py
+++ InsertionSort.py
@@ -1,2 +1,14 @@
-print("Hello World")
+
+A = ([23,4,11,55,2,9,777], 7)
+for i in range(0, 7):
+    current = A[i]
+    j=i
+
+    while j > 0 and current > A[j-1]:
+        temp = A[j-1]
+        A[j-1] = A[j]
+        A[j] = temp
+        j=j-1                        
+
+print(A)
 
