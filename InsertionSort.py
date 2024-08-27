3fed985e14cf267f7ad02d60098fb3700df74964
nbhatta28321 <nbhatta28321@ucumberlands.edu>
2024-08-27 15:45:35 -0400

    updated with function

--- InsertionSort.py
+++ InsertionSort.py
@@ -1,14 +1,15 @@
+def insertion_sort_decresing_order(A, n):
 
-A = ([23,4,11,55,2,9,777], 7)
-for i in range(0, 7):
-    current = A[i]
-    j=i
+    for i in range(0, n):
+        current = A[i]
+        j=i
 
-    while j > 0 and current > A[j-1]:
-        temp = A[j-1]
-        A[j-1] = A[j]
-        A[j] = temp
-        j=j-1                        
+        while j > 0 and current > A[j-1]:
+            temp = A[j-1]
+            A[j-1] = A[j]
+            A[j] = temp
+            j=j-1                        
+  
+    print(A)
 
-print(A)
-
+insertion_sort_decresing_order([23,4,11,55,2,9,777], 7)
